21bbd9f62bf80d4e9e46a0e23f2ae73f1d9c7c00
mlecz <mir_85@mail.com>
2018-01-24 11:55:28 +0100

    Czyste metody

---
+++ src/Artykul.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Artykul {
+
+    private String nazwaArtykulu = "";
+    private double netto = 0;
+    private int vat = 0;
+    private double brutto = netto * vat;
+
+    ArrayList<String> grupyArtykulow = new ArrayList<>();
+    ArrayList<String> jednostkaMagazynowa = new ArrayList<>();
+
+    public void dodaajArtykul(){
+
+    }
+
+    public void dodaajJednostkeMagazynowa(){
+
+    }
+}
