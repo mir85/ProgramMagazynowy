a766716a00f8b6e816d2092a70cf51f2da2945a6
mlecz <mir_85@mail.com>
2018-01-26 11:51:28 +0100

    SQL connection potwierdzone polaczenie

--- src/ProgramMagazynowyTester.java
+++ src/ProgramMagazynowyTester.java
@@ -1,15 +1,63 @@
+import java.sql.*;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ProgramMagazynowyTester {
 
+    static String daneZBazy;
+
     public static void main(String[] args) {
-        Connection connection = null;
 
-        try{
+        String polaczenieURL = "jdbc:mysql://127.0.0.1/samochody?user=root&password=";
+        //Tworzymy proste zapytanie doa bazy danych
+        String query = "Select * FROM klienci";
 
-        }catch (Exception e){
-            e.printStackTrace();
+        Connection conn = null;
+
+        try {
+
+            //Ustawiamy dane dotyczące podłączenia
+            conn = DriverManager.getConnection(polaczenieURL);
+
+            //Ustawiamy sterownik MySQL
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //Uruchamiamy zapytanie do bazy danych
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                wyswietlDaneZBazy(rs);
+            }
+
+            conn.close();
+        }
+        //Wyrzuć wyjątki jężeli nastąpią błędy z podłączeniem do bazy danych lub blędy zapytania o dane
+        catch(ClassNotFoundException wyjatek) {
+            System.out.println("Problem ze sterownikiem");
         }
 
-     }
+        catch(SQLException wyjatek) {
+            //e.printStackTrace();
+            //System.out.println("Problem z logowaniem\nProsze sprawdzic:\n nazwę użytkownika, hasło, nazwę bazy danych lub adres IP serwera");
+            System.out.println("SQLException: " + wyjatek.getMessage());
+            System.out.println("SQLState: " + wyjatek.getSQLState());
+            System.out.println("VendorError: " + wyjatek.getErrorCode());
+        }
+
+    }
+    static void wyswietlDaneZBazy(ResultSet rs){
+        try{
+            daneZBazy = rs.getString(1);
+            System.out.println("\n" + daneZBazy + " ");
+            daneZBazy = rs.getString(2);
+            System.out.println(daneZBazy + " ");
+            daneZBazy = rs.getString(3);
+            System.out.println(daneZBazy);
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
