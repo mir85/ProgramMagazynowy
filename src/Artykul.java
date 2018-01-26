ffbe5bbb4368ec909c3e623a8df168c7fe05dcf2
mlecz <mir_85@mail.com>
2018-01-26 09:48:20 +0100

    Napisane metody

--- src/Artykul.java
+++ src/Artykul.java
@@ -3,25 +3,59 @@ import java.util.Scanner;
 
 public class Artykul {
 
-    private String nazwaArtykulu = "";
-    private double netto = 0;
+    private String nazwaArtykulu;
+    private double netto;
     private int vat = 0;
     private double brutto = netto * vat;
 
-    private ArrayList<String> grupyArtykulow = new ArrayList<>();
-    private ArrayList<String> jednostkaMagazynowa = new ArrayList<>();
+    private String grupaArtykulu;
+
+    ArrayList<String> grupaArtykuluAL = new ArrayList<>();
+
+    public String getGrupaArtykulu() {
+
+        if (!grupaArtykuluAL.contains(grupaArtykulu)){
+            System.out.println("Podaj poprawna grupe badz stworz nowa");
+        }
+        if (grupaArtykuluAL.contains(grupaArtykulu))
+        return grupaArtykulu;
+        return grupaArtykulu;
+    }
+
+    private String jednostkaMagazynowa;
+
+    public Artykul(String nazwaArtykulu, double netto, int vat, String grupaArtykulu, String jednostkaMagazynowa) {
+        this.nazwaArtykulu = nazwaArtykulu;
+        this.netto = netto;
+        this.vat = vat;
+        this.grupaArtykulu = grupaArtykulu;
+        this.jednostkaMagazynowa = jednostkaMagazynowa;
+    }
+
+    public void dodajArtykul(){
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Nazwa artykułu:");
+        nazwaArtykulu = skaner.nextLine();
+        System.out.println("Cena netto:");
+        netto = skaner.nextDouble();
+        System.out.println("Nazwa artykułu:");
+        vat = skaner.nextInt();
+        skaner.close();
+    }
 
     public void dodajJednostkeMagazynowa(){
         Scanner skaner = new Scanner(System.in);
         String grupaArtykulow = skaner.nextLine();
-        grupyArtykulow.add(grupaArtykulow);
+        grupaArtykuluAL.add(grupaArtykulow);
         skaner.close();
     }
 
-    public void dodajJednostke(){
+    /*public void dodajJednostke(){
         Scanner skaner1 = new Scanner(System.in);
         String jednostka = skaner1.nextLine();
         jednostkaMagazynowa.add(jednostka);
         skaner1.close();
     }
+*/
+
 }
---
+++ src/ProgramMagazynowyTester.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+
+public class ProgramMagazynowyTester {
+
+    public static void main(String[] args) {
+        Connection connection = null;
+
+        try{
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+     }
+}
